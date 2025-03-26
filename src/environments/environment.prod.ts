b9ca4648e845c52772440b11c941ab9383839c66
Hugo Eseverri <hugooeseverri@gmail.com>
2025-03-26 13:37:22 -0300

    pre subida a vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiKey: process.env['API_KEY'] || 'b6b082c9f2e8cceb7e7d464cb42e27b6'
+    apiKey: (window as any).API_KEY || 'b6b082c9f2e8cceb7e7d464cb42e27b6'
 };
