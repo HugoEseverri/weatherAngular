bf9130bb5d9e0988acb371016cf3dbc1288bc78e
Hugo Eseverri <hugooeseverri@gmail.com>
2025-03-26 13:40:50 -0300

    arreglos para vercel

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiKey: (window as any).API_KEY || 'b6b082c9f2e8cceb7e7d464cb42e27b6'
+    apiKey: typeof window !== 'undefined' ? (window as any).API_KEY || '' : ''
 };
