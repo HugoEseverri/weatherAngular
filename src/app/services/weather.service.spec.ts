cb32033583fbe077ea95a7fbee02a1c7848b4a1b
Hugo Eseverri <hugooeseverri@gmail.com>
2025-03-27 15:40:24 -0300

    se agregan pruebas de testing con Jasmine

--- src/app/services/weather.service.spec.ts
+++ src/app/services/weather.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { WeatherService } from './weather.service';
+import { environment } from '../../environments/environments';
 
 describe('WeatherService', () => {
   let service: WeatherService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [WeatherService],
+    });
+
     service = TestBed.inject(WeatherService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('debería ser creado', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('debería obtener datos del clima correctamente', (done) => {
+    const mockResponse = {
+      main: { temp: 25 },
+      name: 'Madrid',
+    };
+    const city = 'Madrid';
+    const expectedUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${environment.apiKey}`;
+
+    service.getWeatherByCity(city).subscribe((data) => {
+      expect(data).toEqual(mockResponse);
+      done(); // Asegura que la prueba termina correctamente
+    });
+
+    const req = httpMock.expectOne(expectedUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+  });
+
+  it('debería manejar errores cuando la API falle', (done) => {
+    const city = 'Madrid';
+    const expectedUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${environment.apiKey}`;
+
+    service.getWeatherByCity(city).subscribe({
+      next: () => fail('debería haber fallado'),
+      error: (error) => {
+        expect(error.status).toBe(404);
+        expect(error.statusText).toBe('Not Found');
+        expect(error.message).toContain('Error');
+        done();
+      },
+    });
+
+    const req = httpMock.expectOne(expectedUrl);
+    expect(req.request.method).toBe('GET');
+    req.flush('Error', { status: 404, statusText: 'Not Found' });
   });
 });
